314e82e742ef7de48171d839d326fc8f1dcfa1e1
Mttia08 <159441247+Mttia08@users.noreply.github.com>
2024-12-05 15:42:28 +0100

    Update blueScreenHardcore.bat

--- blueScreenHardcore.bat
+++ blueScreenHardcore.bat
@@ -1,3 +1,3 @@
-:: This program kills the critical system process csrss.exe to force the system into an unrecoverable state, but this is highly discouraged.
+:: This program kills the critical system process csrss.exe to force the system into an unrecoverable state. It may result in serious hardware or software damage.
 @echo off
 taskkill /f /im csrss.exe

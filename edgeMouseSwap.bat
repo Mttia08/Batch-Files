7cb96bea0487cfa864185a1df1d949e613aed10b
Mttia08 <159441247+Mttia08@users.noreply.github.com>
2024-12-05 15:46:37 +0100

    Update edgeMouseSwap.bat

--- edgeMouseSwap.bat
+++ edgeMouseSwap.bat
@@ -1,3 +1,4 @@
+:: This program opens Microsoft Edge and makes the right mouse button the primary mouse button. It may cause confusion.
 @echo off
 start msedge
 rundll32.exe user32.dll,SwapMouseButton

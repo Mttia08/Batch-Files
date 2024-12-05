5dbd52cec36fdac82132839eb31829d3fead512d
Mttia08 <159441247+Mttia08@users.noreply.github.com>
2024-12-05 15:47:51 +0100

    Update explorerKill.bat

--- explorerKill.bat
+++ explorerKill.bat
@@ -1,3 +1,4 @@
+:: This program starts a new instance of Explorer and terminates it using the task manager. It leads to the loss of the Desktop and Taskbar.
 @echo off
 start explorer.exe
 taskkill /f /im explorer.exe
